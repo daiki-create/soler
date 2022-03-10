fcf7e416b9a5b5f2745c6708597b5b1c289d8a24
daiki-create <6280ikiad@gmail.com>
2022-03-10 18:32:52 +0000

    env test

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-die('production');
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
@@ -23,7 +23,7 @@ die('production');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://climate.local.com';
 
 /*
 |--------------------------------------------------------------------------
