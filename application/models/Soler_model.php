368f066f7d26cc61b2eaf6a3cf27df24639b5778
daiki-create <6280ikiad@gmail.com>
2022-04-11 13:25:35 +0000

    aaa

--- application/models/Soler_model.php
+++ application/models/Soler_model.php
@@ -57,9 +57,9 @@ class Soler_model extends CI_Model
         //   }
     }
 
-    public function xlsxToCsv()
+    public function xlsxToCsv($n)
     {
-        $xls_files = glob($this->soler_dir . "/public/xlsx/*.xlsx");
+        $xls_files = glob($this->soler_dir . "/public/xlsx/0".$n."*.xlsx");
 
         foreach($xls_files as $xf)
         {
@@ -77,9 +77,9 @@ class Soler_model extends CI_Model
         return TRUE;
     }
 
-    public function saveCurrentSoler()
+    public function saveCurrentSoler($n)
     {
-        $csv_files = glob($this->soler_dir . "/public/csv/*.csv");
+        $csv_files = glob($this->soler_dir . "/public/csv/0".$n."*.csv");
         foreach($csv_files as $cf)
         {
             // 読み込むCSVファイルを指定
@@ -123,7 +123,7 @@ class Soler_model extends CI_Model
     public function getAggregatedSoler()
     {
         $soler_aggregated_array = [];
-        $prefectures = ["全国","北海道","青森"];
+        $prefectures = ['全国','北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県','新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県','鳥取県','島根県','岡山県','広島県','山口県','徳島県','香川県','愛媛県','高知県','福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県'];
         foreach($prefectures as $prefecture)
         {
             $soler = $this->Soler_tbl->getAllSoler($prefecture);
