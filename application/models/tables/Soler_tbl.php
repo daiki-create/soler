0cb18795b24a92fa10904041f81dd6cfd5c70794
daiki-create <6280ikiad@gmail.com>
2022-04-16 19:45:46 +0000

    検索条件改善

--- application/models/tables/Soler_tbl.php
+++ application/models/tables/Soler_tbl.php
@@ -172,27 +172,61 @@ class Soler_tbl extends CI_Model
            $sql = $sql->like('facility_adress', $area);
          }
 
+        //  重複　なし
          if($unique)
          {
-            if(!($adress_blank == "blank"))
+            // 住所記　載指定なし
+            if($adress_blank == 0)
             {
+                // adress空白 and nameユニーク または　adressTRUE and adressユニーク
+                $sql = $sql->group_by('adress');
+                $sql = $sql->group_by('name');
+            }
+            // 住所記載　あり
+            if($adress_blank == "no_blank")
+            {
+                $sql = $sql->where('adress !=', '');
                 $sql = $sql->group_by('adress');
             }
+            // 住所記載　未記入
+            if($adress_blank == "blank")
+            {
+                $sql = $sql->group_by('name');
+            }
          }
+        //  重複　あり
+         else
+         {
+             // 住所記　載指定なし
+            if($adress_blank == 0)
+            {
+                // なにもしない
+            }
+            // 住所記載　あり
+            if($adress_blank == "no_blank")
+            {
+                $sql = $sql->where('adress !=', '');
+            }
+            // 住所記載　未記入
+            if($adress_blank == "blank")
+            {
+                $sql = $sql->where('adress =', '');
+            }
+        }
+         
+        //  if($adress_blank == "blank")
+        //  {
+        //     $sql = $sql->where('adress =', '');
+        //     $sql = $sql->group_by('name');
+        //  }
+        //  elseif($adress_blank == "no_blank")
+        //  {
+        //     $sql = $sql->where('adress !=', '');
+        //  }
 
          if($output)
          {
             $sql = $sql->where('output <=', 50);
-         }
-         
-         if($adress_blank == "blank")
-         {
-            $sql = $sql->where('adress =', '');
-            $sql = $sql->group_by('name');
-         }
-         elseif($adress_blank == "no_blank")
-         {
-            $sql = $sql->where('adress !=', '');
          }
 
          $sql = $sql
