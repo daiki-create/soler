18b86b816160c2052d376b9eb0d06f3ee8f4f360
daiki-create <6280ikiad@gmail.com>
2022-04-21 04:04:15 +0000

    都道府県追加　ソート機能追加

--- application/models/Soler_model.php
+++ application/models/Soler_model.php
@@ -295,6 +295,12 @@ class Soler_model extends CI_Model
                             $city = "杵築市";
                             $adress = $prec . $city . str_replace('杵築', '', $adress);
                         }
+                        elseif(preg_match('/高萩/', $adress))
+                        {
+                            $prec = "茨城県県";
+                            $city = "高萩市";
+                            $adress = $prec . $city . str_replace('高萩', '', $adress);
+                        }
                         elseif(preg_match('/(北海道|青森|岩手|宮城|秋田|山形|福島|茨城|栃木|群馬|埼玉|千葉|東京|神奈川|新潟|富山|石川|福井|山梨|長野|岐阜|静岡|愛知|三重|滋賀|[^東]京都|大阪|兵庫|奈良|和歌山|鳥取|島根|岡山|広島|山口|徳島|香川|愛媛|高知|福岡|佐賀|長崎|熊本|大分|宮崎|鹿児島|沖縄)/', $adress, $matches5))
                         {
                             $prec = $matches5[0];
