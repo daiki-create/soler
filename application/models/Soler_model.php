3b6f8ee171ea9f5d2c5cf66599b9d308768052f7
daiki-create <6280ikiad@gmail.com>
2022-04-22 04:41:10 +0000

    都道府県追加　ソート機能追加

--- application/models/Soler_model.php
+++ application/models/Soler_model.php
@@ -578,8 +578,12 @@ class Soler_model extends CI_Model
                 $csv_content = $csv_content . $new_row;
             }
             // csvに書き込み保存
-            // file_put_contents($this->soler_dir . "/public/csv_add_prec_to_adress/" . $file_name, $csv_content);
-            file_put_contents($this->soler_dir . "/public/csv_add_prec_to_adress/test.csv");
+            // if(!file_put_contents($this->soler_dir . "/public/csv_add_prec_to_adress/" . $file_name, $csv_content))
+            if(!file_put_contents($this->soler_dir . "/public/csv_add_prec_to_adress/test.csv"))
+            {
+                echo "<br>ファイル書き込み失敗";
+                return FALSE;
+            }
             exit;
         }
         return TRUE;
